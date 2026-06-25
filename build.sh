a107f8f4169da60277f1c33ea09bf1edce72ec69
struc2ture <struc0ture@gmail.com>
2026-06-25 15:17:22 -0700

    Clean up files.

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-LIB_SRC="src/mem_U.cpp"
-
 EX_NAME="${1}"
 EX_SRC="examples/${EX_NAME}.cpp"
 EX_OUT="bin/${EX_NAME}"
@@ -9,8 +7,8 @@ EX_OUT="bin/${EX_NAME}"
 clang++ \
     -g -std=c++23 \
     -Wall -Wextra \
-    $LIB_SRC \
     $EX_SRC \
+    src/mem_provider.cpp \
     -o $EX_OUT \
     -I/Users/struc/dev/concepts/mem/src \
     -fno-strict-aliasing
--- src/mem_provider.cpp
+++ src/mem_provider.cpp
@@ -1,4 +1,4 @@
-#pragma once
+#include "mem_provider.h"
 
 #include <cstddef>
 #include <cstdio>
